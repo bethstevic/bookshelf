59c6ef3a31b168252fd083888c334948164ef9d6
Beth Stevic <bethstevic89@gmail.com>
2017-05-01 17:34:33 -0700

    started implementing angular

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-//this will handle angular interactions with html and server
+angular.module('book-list', [])
+.controller('ToRead', function($scope) {
+  $scope.booksToRead = [
+    {title: 'test book', id: 1},
+    {title: 'test book 2', id: 2}
+  ];
+
+  this.inputResults = (data) => {
+    booksToRead.push(data);
+  };
+
+});
