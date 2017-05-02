c502c6aacccd53bdc90fd7ec1981615cb54ac076
Beth Stevic <bethstevic89@gmail.com>
2017-05-01 20:14:35 -0700

    started setting up ajax methods to connect client and server

--- public/app.js
+++ public/app.js
@@ -1,17 +1,30 @@
 angular.module('book-list', [])
-.controller('ToRead', function($scope) {
+.controller('ToRead', function($scope, $http) {
   $scope.booksToRead = [
     {title: 'test book', id: 1},
     {title: 'test book 2', id: 2}
   ];
 
-  this.inputResults = (data) => {
-    booksToRead.push(data);
-  };
+  $scope.updateToRead = (newBook) => {
+    console.log(newBook);
+  //   booksToRead.insert({
+  //     title: newBook,
+  //     id: 3
+  //   });
+  //   this.newBook = '';
+   };
+
+   $http.get('something_not_sure_yet')
+   .then(function (res) {
+      $scope.content = res.data;
+   }, function (res) {
+    $scope.content = 'Something went wrong';
+   });
 
 })
 .directive('eachBook', function() {
   return {
+
     template: 'Title: {{book.title}} Id: {{book.id}}'
   };
 });
